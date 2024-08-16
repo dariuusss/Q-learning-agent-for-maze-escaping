a7966651a64342ce1acb5fdce3d7e13609919a5b
dariuusss <119854575+dariuusss@users.noreply.github.com>
2024-08-16 17:08:36 +0300

    live score done

--- path.py
+++ path.py
@@ -6,6 +6,7 @@ def find_best_path(start,finish,labyrinth,Q):
     state = start
     path = [state]
     visited = set()
+    score_list = [0]
     score = 0
     while state != finish:
         visited.add(state)
@@ -17,6 +18,7 @@ def find_best_path(start,finish,labyrinth,Q):
             action = random.choice(actions)  # Fall back to a valid action if the chosen action is invalid
         x, y = state[0], state[1]
         score = score + Q[state[0], state[1],action]
+        score_list.append(score)
         state = asr.next_state(state, action)
         path.append(state)
-    return (path,score,labyrinth)
+    return (path,score,labyrinth,score_list)
--- visualiser.py
+++ visualiser.py
@@ -2,14 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def interpolate_coordinates(start, end, steps):
-   
-    x_coords = np.linspace(start[1], end[1], steps)
-    y_coords = np.linspace(start[0], end[0], steps)
-    return list(zip(y_coords, x_coords))
+    # Functie de interpolare simpla
+    lin_space = np.linspace(start[0], end[0], steps)
+    col_space = np.linspace(start[1], end[1], steps)
+    return list(zip(lin_space, col_space))
 
-
-def plot_live_maze(matrix, coordinates, interval, steps):
-
+def plot_live_maze(matrix, coordinates, interval, steps, score_list):
     plt.figure(figsize=(19.1, 11))  # Dimensiune mare pentru figura
 
     state_matrix = np.ones(matrix.shape)
@@ -17,6 +15,7 @@ def plot_live_maze(matrix, coordinates, interval, steps):
     for i in range(len(coordinates) - 1):
         start = coordinates[i]
         end = coordinates[i + 1]
+        score = score_list[i]
 
         interpolated_coords = interpolate_coordinates(start, end, steps)
 
@@ -42,6 +41,36 @@ def plot_live_maze(matrix, coordinates, interval, steps):
                 plt.plot([-0.5, matrix.shape[1] - 0.5], [y - 0.5, y - 0.5], color='black', linewidth=1)
 
             plt.title(f'Mișcare de la ({start[0]},{start[1]}) la ({end[0]},{end[1]})')
+
+            plt.text(1.0, 1.05, f'live score : {score:.2f}',
+                     fontsize=20, color='blue', ha='right', va='bottom',
+                     transform=plt.gca().transAxes)
+
             plt.pause(interval)
+
+    plt.clf()
+    plt.imshow(state_matrix, cmap='Greens', interpolation='nearest', aspect='auto')
+    plt.scatter(col, lin, s=500, color='red', edgecolor='black')
+
+    for (i, j), value in np.ndenumerate(matrix):
+        plt.text(j, i, f'{value:.2f}', color='black', ha='center', va='center', fontsize=20)
+
+    plt.xticks(range(matrix.shape[1]))
+    plt.yticks(range(matrix.shape[0]))
+
+    plt.xlim(-0.5, matrix.shape[1] - 0.5)
+    plt.ylim(matrix.shape[0] - 0.5, -0.5)
+
+    for x in range(matrix.shape[1] + 1):
+        plt.plot([x - 0.5, x - 0.5], [-0.5, matrix.shape[0] - 0.5], color='black', linewidth=1)
+
+    for y in range(matrix.shape[0] + 1):
+        plt.plot([-0.5, matrix.shape[1] - 0.5], [y - 0.5, y - 0.5], color='black', linewidth=1)
+
+
+    score = score_list[-1]
+    plt.text(1.0, 1.05, f'live score : {score:.2f}',
+             fontsize=20, color='blue', ha='right', va='bottom',
+             transform=plt.gca().transAxes)
 
     plt.show()
